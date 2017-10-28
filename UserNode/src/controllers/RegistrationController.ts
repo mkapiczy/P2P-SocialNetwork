0363a5a6c7e7d446f7de010407b9dc9f7eb5acaa
Michał Kapiczyński <mix.kap@gmail.com>
2017-10-28 13:56:12 +0200

    Revert "Removing useless components from previous project."
    This reverts commit 99abf02a97b98f0d9e37817f7fd0386406b23555.

--- UserNode/src/controllers/RegistrationController.ts
+++ UserNode/src/controllers/RegistrationController.ts
@@ -15,33 +15,6 @@ class RegistrationController {
     };
 
     post(request, response){
-
-        // we get the form data from the view
-
-        // generate public private key
-
-        // find the user with provided id to acknowledge the data
-
-        // ask him for his public key
-
-        // sign our data with our private key - our signature
-
-        // sign our signature + public ket with his public key
-
-        // We send the data to him, and await response
-
-        // He gets it he decryptsthe data with his private key.
-
-        // So now he has our public key and our signed data.
-
-        // Decrypts our signature with our public key
-
-        // Confirms the data by signing it with his private key.
-
-        // Return the confirmed data to us
-
-        // We publish it to the network. Providing in meta-data our public key and the id of acknowledging node.
-
         console.log("Registration post received");
     };
 
