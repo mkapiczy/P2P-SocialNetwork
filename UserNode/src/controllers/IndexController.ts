99abf02a97b98f0d9e37817f7fd0386406b23555
Michał Kapiczyński <mix.kap@gmail.com>
2017-10-28 11:36:10 +0200

    Removing useless components from previous project.

--- UserNode/src/controllers/IndexController.ts
+++ UserNode/src/controllers/IndexController.ts
@@ -5,20 +5,10 @@ class IndexController {
 
     constructor() {
         this.router.get("/", this.indexView);
-        this.router.get("/pug", this.indexPug);
     }
 
     indexView(request, response) {
         response.sendFile('../views.index.html');
-    };
-
-    indexPug(request, response){
-        response.render("index", {
-            title: "Hey!",
-            message: "This works :-)",
-            node: global.node,
-            buckets: global.BucketManager.buckets
-        });
     };
 
 }
