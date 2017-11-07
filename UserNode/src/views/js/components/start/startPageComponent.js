4d0120d9d712b2160069276335e908c3583bd0da
Lasse Have Nielsen <lasse@havenielsen.dk>
2017-11-07 17:30:18 +0100

    routing works. Nice little badge for notifications - needs an ng-if == 0, but will be implemented when endpoint works.

--- UserNode/src/views/js/components/start/startPageComponent.js
+++ UserNode/src/views/js/components/start/startPageComponent.js
@@ -7,11 +7,12 @@ angular.module('socialApp')
 
         },
 
-        controller: ['$http', function($http){
+        controller: ['$http', '$cookies', function($http, $cookies){
             this.numberOfMessages = 0;
+            console.log($cookies.get("username")),
             this.$onInit = function() {
                 $http.get("http://localhost:8000/data/ack/pending_messages", {
-                    //username: $cookies.get("username"),
+                    username: $cookies.get("username")
                 }).then(function (data) {
                     console.log("Response from server:" + data.data.messages.length);
                     this.numberOfMessages = data.data;
